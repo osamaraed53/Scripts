148454f60171ded91d4103c08d0168ed5ebdfca5
Osama Raed <osama@ascot.ws>
2026-05-10 10:35:43 +0300

    2026-05-10 variables

---
+++ Variables/variables.ps1
@@ -0,0 +1,40 @@
+# $camelCasing
+
+#strings
+$myVariableOne = "Osama Alnobani"
+$myVariableTwo = 'Osama Alnobani'
+
+$myVariableOne
+$myVariableTwo
+
+#numbers
+
+$myVariable3 = 5
+$myVariable4 = 10
+# we can make this operations (+, -, /, * ,%, ++, --  )
+$myVariable5 = $myVariable3 + $myVariable4
+
+$myVariable5
+
+# bool
+
+#comparison operators 
+# Operator	Meaning
+# -eq	Equal
+# -ne	Not equal
+# -gt	Greater than
+# -lt	Less than
+# -ge	Greater or equal
+# -le	Less or equal
+
+# Operator	Meaning
+# -and	AND
+# -or	OR
+# -not	NOT
+
+$condition = $true
+
+$myVariable5 = $myVariable3 -eq $myVariable4
+
+$myVariable5
+$condition
