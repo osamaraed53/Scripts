af0e186dbcf0ec05bacd5d2c209d192d3b4c931a
Osama Raed <osama@ascot.ws>
2026-05-11 15:37:40 +0300

    2026-05-11 Pipline

---
+++ 05-Pipline/pipline.ps1
@@ -0,0 +1,25 @@
+
+$serviceName = "Spooler"
+
+Get-Service -Name $serviceName
+
+Start-Service -Name $serviceName 
+
+# Get-Help Stop-Service -Full
+
+# (Get-Service -Name $serviceName).GetType()
+
+Get-service -Name $serviceName  | Stop-Service
+
+$serviceName | Get-service | Start-Service
+
+
+##################################
+
+$services = New-Object -TypeName System.Collections.ArrayList
+$services.AddRange(@("spooler","w32time"))
+
+$services | Get-Service | Stop-Service
+$services | Get-Service | Start-Service
+
+$services | Get-service | ForEach-Object{Write-Output "Service is : $($_.DisplayName) currently $($_.Status)"}
