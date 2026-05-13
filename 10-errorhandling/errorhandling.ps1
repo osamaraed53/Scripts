44afcb4cb25048d9715cd954e2c871698546d779
Osama Raed <osamaraed53@gmail.com>
2026-05-13 23:48:25 +0300

    2026-05-13 Error handling

---
+++ 10-errorhandling/errorhandling.ps1
@@ -0,0 +1,46 @@
+
+# $filePath = "10-errorhandlin"
+# $files = Get-ChildItem -Path $filePath
+
+# $files.foreach({
+#         Write-Output $_.Name
+#     })
+
+# Write-Output "this after error messages"
+
+
+# try {
+#     $filePath = "10-errorhandlin"
+#     $files = Get-ChildItem -Path $filePath -ErrorAction Stop
+#      #Continue is defult 
+
+#     $files.foreach({
+#             Write-Output $_.Name
+#         })
+
+#     Write-Output "this after error messages"
+# }
+# catch {
+#     Write-Output "Caught Error $_.Exception.Message"
+# }
+
+$ErrorActionPreference = "Stop" # is a global variable that controls how PowerShell handles non-terminating errors for the entire session or script by default is continue . 
+
+try {
+    $filePath = "10-errorhandling"
+    $files = Get-ChildItem -Path $filePath 
+     #Continue is defult 
+
+    $files.foreach({
+            Write-Output $_.Name
+        })
+
+    Write-Output "this after error messages"
+}
+catch {
+    Write-Output "Caught Error $_.Exception.Message"
+}
+finally{
+    Write-Output "This always run not matter what!"
+}
+
