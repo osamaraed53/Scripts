4bb0f894459297504b90725ce4fd13c63fe3d30d
Osama Raed <osamaraed53@gmail.com>
2026-05-16 14:32:30 +0300

    2026-05-6

---
+++ 012-Methods/methods.ps1
@@ -0,0 +1,61 @@
+# function Verb-Noun
+
+function Create-Configration {   
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory ,ValueFromPipeline, ValueFromPipelineByPropertyName,ValueFromRemainingArguments)]
+        [string]
+        $Name,
+        [Parameter()]
+        [string]
+        $Version = "1.0.0",
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Path,  
+        [parameter()]
+        [string]
+        [ValidateSet("Linux", "Windows")]
+        $Os = "Windows"
+    ) 
+
+    begin {
+        Write-Verbose "Begin Block" # verbose is telling the user what is going on 
+        # you can use it to open connection with database
+        $CounterPassed = 0 
+        $CounterFailed = 0
+    }
+
+    process {
+        try {
+        
+            Write-Verbose "From my function $Name with $Version , Path : $Path"
+            New-Item -Path "$Path" -Name "$Name.cfg" -ItemType File -ErrorAction Stop
+            $Version | Out-File -FilePath "$Path\$($Name).cfg"
+            $os | Out-File -FilePath "$path\$($Name).cfg" -Append -Force
+            $CounterPassed++
+        }
+        catch {
+            Write-Output "From catch"
+            Write-Output $_.Exception.Message
+            $CounterFailed++
+        }
+        Write-Debug "Configration Created : $CounterPassed , Configration Failed : $CounterFailed"   
+    }
+
+
+    end {
+        Write-Verbose "End block"    
+        # you can use it to close connection with database    
+        Write-Output "Configration Created : $CounterPassed , Configration Failed : $CounterFailed"
+    }
+}
+
+
+
+$Names = @("Test01", "Test02", "Test03","Test04")
+
+
+$Names | Create-Configration -Path "012-Methods\Configs" -Version "1.0.2" -os Linux -Verbose -Debug
+
+
+# Get-ChildItem -Path "012-Methods\Configs" | Remove-Item
--- 10-errorhandling/errorhandling.ps1
+++ 10-errorhandling/errorhandling.ps1
@@ -9,20 +9,20 @@
 # Write-Output "this after error messages"
 
 
-# try {
-#     $filePath = "10-errorhandlin"
-#     $files = Get-ChildItem -Path $filePath -ErrorAction Stop
-#      #Continue is defult 
+try {
+    $filePath = "10-errorhandlin"
+    $files = Get-ChildItem -Path $filePath -ErrorAction Stop
+     #Continue is defult 
 
-#     $files.foreach({
-#             Write-Output $_.Name
-#         })
+    $files.foreach({
+            Write-Output $_.Name
+        })
 
-#     Write-Output "this after error messages"
-# }
-# catch {
-#     Write-Output "Caught Error $_.Exception.Message"
-# }
+    Write-Output "this after error messages"
+}
+catch {
+    Write-Output "Caught Error $_.Exception.Message"
+}
 
 $ErrorActionPreference = "Stop" # is a global variable that controls how PowerShell handles non-terminating errors for the entire session or script by default is continue . 
 
